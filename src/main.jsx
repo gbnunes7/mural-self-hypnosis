cb3b39aa467c11269ef9e4749260312b12f6586f
Gabriel Melo <bielrj170@gmail.com>
2024-08-26 17:00:26 -0300

    chorus: structure project folder

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import '/src/styles/index.css'
+import AppRoutes from './routes/AppRoutes.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <AppRoutes />
   </StrictMode>,
 )
