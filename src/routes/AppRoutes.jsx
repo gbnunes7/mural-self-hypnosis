d42d10743bd6df3d0afe96ff91466a5426215c89
Gabriel Melo <bielrj170@gmail.com>
2024-08-27 15:26:13 -0300

    feat: finalize router structure to navigate between pages

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,8 +1,20 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import InitialLayout from "../components/initialLayout";
+import Home from "../pages/home";
+
 function AppRoutes() {
-  return (
-    <>
-    </>
-  )
+	return (
+		<>
+			<BrowserRouter>
+				<Routes>
+					<Route path="/" element={<InitialLayout />}>
+						<Route path="/" element={<Home/>}/>
+						<Route path="/mural" element={<div><p>Nada encontrado</p></div>}/> 
+					</Route>
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
 }
 
-export default AppRoutes
+export default AppRoutes;
