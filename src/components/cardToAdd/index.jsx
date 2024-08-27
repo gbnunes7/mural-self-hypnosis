b4d023d3062063fa2cae669d1fc936e0dde021d5
Gabriel Melo <bielrj170@gmail.com>
2024-08-26 23:36:10 -0300

    feat: add a card form component

--- src/components/cardToAdd/index.jsx
+++ src/components/cardToAdd/index.jsx
@@ -1,25 +1,40 @@
+import Button from "../button";
 import InputForm from "../input";
 import Title from "../title";
 
 const CardToAdd = () => {
 	return (
-		<form id="form-pensamento" className="bg-red-500 flex flex-col py-10 px-8 min-w-[792px] min-h-[350px] rounded-2xl shadow-xl shadow-[#04183214] gap-10">
+		<form id="form-pensamento" className="bg-white flex flex-col py-10 px-8 min-w-[700px] min-h-[350px] rounded-2xl shadow-xl shadow-[#04183214] gap-10">
 			<div className="flex flex-col gap-4">
 				<Title>
 					<h2>Autosugestão ou Ideia</h2>
 				</Title>
-				<InputForm placeholder={"Digite aqui sua autosugestão..."} height={'112px'} name={'Autosugestão'}/>
+				<InputForm
+					placeholder={"Digite aqui sua autosugestão..."}
+					height={"112px"}
+					name={"autosugestão"}
+				/>
 			</div>
 			<div className="flex flex-col gap-4">
 				<Title>
 					<h2>Autor(a)</h2>
 				</Title>
-				<InputForm placeholder={"Digite aqui o autor ou atora desse pensamento ou ideia."} height={'40px'} name={'Autor'}/>
+				<InputForm
+					placeholder={
+						"Digite aqui o autor ou atora desse pensamento ou ideia."
+					}
+					height={"40px"}
+					name={"autor"}
+				/>
 			</div>
-            <div className="flex justify-center">
-            <button>oi</button>
-            <button>oi</button>
-            </div>
+			<div className="flex justify-center gap-4">
+				<Button background={"#041832"} color={"white"}>
+					Adicionar
+				</Button>
+				<Button background={"white"} color={"#041832"}>
+					Cancelar
+				</Button>
+			</div>
 		</form>
 	);
 };
