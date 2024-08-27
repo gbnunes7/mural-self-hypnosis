1fdac882cdc86da8085f5883608bccb8390b30c5
Gabriel Melo <bielrj170@gmail.com>
2024-08-26 22:45:35 -0300

    style: delete obsolete import

--- src/components/title/index.jsx
+++ src/components/title/index.jsx
@@ -1,7 +1,6 @@
-import { Children } from "react";
 
-const Title = () => {
-	return <div className="text-2xl text-[#041832]">{Children}</div>;
+const Title = ({children}) => {
+	return <div className="text-2xl text-[#041832]">{children}</div>;
 };
 
 export default Title
