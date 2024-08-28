25f4ba0abf10559b46ef4305ba60e31906df8a6e
Gabriel Melo <bielrj170@gmail.com>
2024-08-28 05:58:53 -0300

    fix: fix bug get, create a new state to data

--- src/context/index.jsx
+++ src/context/index.jsx
@@ -5,10 +5,12 @@ export const CardContext = createContext();
 const CardProvider = ({ children }) => {
 	const [pensamento, setPensamento] = useState("");
 	const [autor, setAutor] = useState("");
+	const [data, setData] = useState([]);
+
 
 	return (
 		<CardContext.Provider
-			value={{ pensamento, setPensamento, autor, setAutor }}
+			value={{ pensamento, setPensamento, autor, setAutor,data,setData }}
 		>
 			{children}
 		</CardContext.Provider>
