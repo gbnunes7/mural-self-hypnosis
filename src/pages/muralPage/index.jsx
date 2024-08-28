d56b4ab581e47649d0d682703ec2ed9fda8eb9a8
Gabriel Melo <bielrj170@gmail.com>
2024-08-28 06:39:32 -0300

    style: fix indentation

--- src/pages/muralPage/index.jsx
+++ src/pages/muralPage/index.jsx
@@ -4,12 +4,15 @@ import useCardContext from "../../hooks";
 import { IoMdArrowBack } from "react-icons/io";
 
 const MuralPage = () => {
-	const { data, handleDelete, handleEdit,navigate } = useCardContext();
+	const { data, handleDelete, handleEdit, navigate } = useCardContext();
 
 	return (
 		<div className="flex flex-grow flex-col gap-4 py-14 items-center bg-[url('/public/images/image-background.svg')] bg-cover bg-center bg-no-repeat">
-			<button className="relative top-11 right-1/3" onClick={() => navigate(-1)}>
-				<IoMdArrowBack  />
+			<button
+				className="relative top-11 right-1/3"
+				onClick={() => navigate(-1)}
+			>
+				<IoMdArrowBack />
 			</button>
 			<Title>Meu mural</Title>
 			<div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 w-full max-w-[1200px]">
