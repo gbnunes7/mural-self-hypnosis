caa2b0b9bd32f425af43c80d8b19c7d3a223a959
Gabriel Melo <bielrj170@gmail.com>
2024-08-28 06:29:58 -0300

    style: add a hover effect on button

--- src/components/button/index.jsx
+++ src/components/button/index.jsx
@@ -1,6 +1,6 @@
 const Button = ({children, background,color,type}) => {
     return (
-        <button type={type} className="py-2 px-6 w-[153px] rounded-[32px] text-[20px] text-center border border-[#041832]" style={{backgroundColor:background, color:color}}>
+        <button type={type} className="py-2 px-6 w-[153px] rounded-[32px] text-[20px] text-center border border-[#041832] transition-transform duration-300 transform hover:scale-110" style={{backgroundColor:background, color:color} }>
             {children}
         </button>
     )
